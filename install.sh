e82e877a17d64d9fb8f96296d12c729881f1758e
Michal Zimmermann <zimmicz@gmail.com>
2015-11-07 11:44:52 +0100

    Move files to ~/dotfiles, if not already present

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# if dotfiles doesn't exist in the home directory,
+# create it and move files to that directory
+if [[ ! -d ~/dotfiles ]]; then
+    mkdir ~/dotfiles
+    find ./ ! -name "*.sh" -exec cp {} ~/dotfiles \;
+fi
 
+# create symlinks to home folder
 for f in $(find ~/dotfiles/* ! -name "*.sh"); do
     FILE=$(basename $f)
     echo installing $FILE...
